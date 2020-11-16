769600fbc67a18a53c2f98f711aabb1d566d3872
Matioupi <mathieu.peyrega@gmail.com>
2020-11-16 13:21:00 +0100

    Misc merges for prod branch

--- ports/proj4/portfile.cmake
+++ ports/proj4/portfile.cmake
@@ -1,17 +1,19 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO OSGeo/PROJ
-    REF 6.3.1
-    SHA512 ec5a2b61b12d3d3ec2456b9e742cf7be98767889c4759334e60276f609054fa8eb59f13f07af38e69e9ee7b6f2b9542e2d5d7806726ce5616062af4de626c6fa
+    REF 7.1.1
+    SHA512 78e51a054bdf87a2c815b4f83452b4e0ec2ca9a8375d8ef22325550ea1ff96a0ed3efb967c98853dbdda05331b181034ef0a09632957fecd7d52ef33aebc0ff4
     HEAD_REF master
     PATCHES
         fix-sqlite3-bin.patch
-        disable-export-namespace.patch
-        disable-projdb-with-arm-uwp.patch
-        fix-win-output-name.patch
-        fix-sqlite-dependency-export.patch
-        fix-linux-build.patch
-        use-sqlite3-config.patch
+		fix_default_datadir.patch
+		#fix-static-bin-cct.patch
+		#fix-static-bin-cs2cs.patch
+		#fix-static-bin-geod.patch
+		#fix-static-bin-gie.patch
+		#fix-static-bin-proj.patch
+		#fix-static-bin-projinfo.patch
+		#fix-static-bin-projsync.patch	
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
@@ -45,28 +47,117 @@ if ("database" IN_LIST FEATURES)
     endif()
 endif()
 
-vcpkg_configure_cmake(
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+	tool BUILD_CCT
+	tool BUILD_CS2CS
+	tool BUILD_GEOD
+	tool BUILD_GIE
+	tool BUILD_PROJ
+	tool BUILD_PROJINFO
+	tool BUILD_PROJSYNC
+	)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+	if (VCPKG_TARGET_IS_WINDOWS)
+		vcpkg_acquire_msys(MSYS_ROOT)
+		set(PKGCONFIG ${MSYS_ROOT}/usr/bin/pkg-config.exe)
+	elseif (VCPKG_TARGET_IS_LINUX)
+		set(PKGCONFIG pkg-config)
+	endif()
+	
+	set(CURL_PKGCONFIG_OTHERLIBS_FILE "pkgconfig-libcurl-onlyother-${TARGET_TRIPLET}")
+	vcpkg_execute_required_process(COMMAND ${PKGCONFIG} --libs-only-other ${CURRENT_INSTALLED_DIR}/lib/pkgconfig/libcurl.pc
+								   WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+								   LOGNAME "${CURL_PKGCONFIG_OTHERLIBS_FILE}-rel")
+	vcpkg_execute_required_process(COMMAND ${PKGCONFIG} --libs-only-other ${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig/libcurl.pc
+								   WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+								   LOGNAME "${CURL_PKGCONFIG_OTHERLIBS_FILE}-dbg")
+								   
+	set(CURL_PKGCONFIG_SYSTEMLIBS_FILE "pkgconfig-libcurl-onlysystem-${TARGET_TRIPLET}")
+	
+	vcpkg_execute_required_process(COMMAND ${PKGCONFIG} --libs-only-l ${CURRENT_INSTALLED_DIR}/lib/pkgconfig/libcurl.pc
+								   WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+								   LOGNAME "${CURL_PKGCONFIG_SYSTEMLIBS_FILE}-rel")
+								   
+	vcpkg_execute_required_process(COMMAND ${PKGCONFIG} --libs-only-l ${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig/libcurl.pc
+								   WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+								   LOGNAME "${CURL_PKGCONFIG_SYSTEMLIBS_FILE}-dbg")
+								   
+	FILE(STRINGS "${CURRENT_BUILDTREES_DIR}/${CURL_PKGCONFIG_OTHERLIBS_FILE}-rel-out.log" CURL_PKGCONFIG_OTHERLIBS)
+	string(APPEND VCPKG_LINKER_FLAGS_RELEASE "${CURL_PKGCONFIG_OTHERLIBS}")
+
+	FILE(STRINGS "${CURRENT_BUILDTREES_DIR}/${CURL_PKGCONFIG_OTHERLIBS_FILE}-dbg-out.log" CURL_PKGCONFIG_OTHERLIBS)
+	string(APPEND VCPKG_LINKER_FLAGS_DEBUG "${CURL_PKGCONFIG_OTHERLIBS}")
+
+	FILE(STRINGS "${CURRENT_BUILDTREES_DIR}/${CURL_PKGCONFIG_SYSTEMLIBS_FILE}-rel-out.log" CURL_PKGCONFIG_SYSTEMLIBS)
+	string(REPLACE " " ";" CURL_PKGCONFIG_SYSTEMLIBS ${CURL_PKGCONFIG_SYSTEMLIBS} )
+	foreach(sl ${CURL_PKGCONFIG_SYSTEMLIBS})
+		string(FIND "${sl}" "curl" tmp_res)
+		if (${tmp_res} EQUAL -1)
+			string(REPLACE "-l" "" sl ${sl})
+			message(STATUS "${sl}${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX}")
+			string(APPEND VCPKG_LINKER_FLAGS_RELEASE " ${sl}${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX}")
+		else()
+			message(STATUS "was curl")
+		endif()			
+	endforeach()
+
+	FILE(STRINGS "${CURRENT_BUILDTREES_DIR}/${CURL_PKGCONFIG_SYSTEMLIBS_FILE}-dbg-out.log" CURL_PKGCONFIG_SYSTEMLIBS)
+	string(REPLACE " " ";" CURL_PKGCONFIG_SYSTEMLIBS ${CURL_PKGCONFIG_SYSTEMLIBS} )
+	foreach(sl ${CURL_PKGCONFIG_SYSTEMLIBS})
+		#message(STATUS "test string : ${sl}")
+		string(FIND "${sl}" "curl" tmp_res)
+		#message(STATUS "result : ${tmp_res}")
+		if (${tmp_res} EQUAL -1)
+			string(REPLACE "-l" "" sl ${sl})
+			message(STATUS "${sl}${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX}")
+			string(APPEND VCPKG_LINKER_FLAGS_DEBUG " ${sl}${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX}")
+		else()
+			message(STATUS "was curl")
+		endif()
+	endforeach()
+	
+	message(STATUS "additionnal debug linker flag : ${VCPKG_LINKER_FLAGS_DEBUG}")
+	message(STATUS "additionnal release linker flag : ${VCPKG_LINKER_FLAGS_RELEASE}")
+	message(STATUS "additionnal linker flag : ${VCPKG_LINKER_FLAGS}")
+endif()
+
+#if (VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL  "static")
+#	string(APPEND VCPKG_LINKER_FLAGS "ws2_32.lib wldap32.lib crypt32.lib")
+#endif()
+
+vcpkg_configure_cmake( 
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS ${FEATURE_OPTIONS}
     -DBUILD_LIBPROJ_SHARED=${VCPKG_BUILD_SHARED_LIBS}
     -DPROJ_LIB_SUBDIR=lib
     -DPROJ_INCLUDE_SUBDIR=include
-    -DPROJ_DATA_SUBDIR=share/proj4
-    -DBUILD_CCT=OFF
-    -DBUILD_CS2CS=OFF
-    -DBUILD_GEOD=OFF
-    -DBUILD_GIE=OFF
-    -DBUILD_PROJ=OFF
-    -DBUILD_PROJINFO=OFF
+    -DPROJ_DATA_SUBDIR=${CURRENT_INSTALLED_DIR}/share/proj4
     -DPROJ_TESTS=OFF
     -DEXE_SQLITE3=${SQLITE3_BIN_PATH}/sqlite3${BIN_SUFFIX}
 )
 
-vcpkg_install_cmake()
+if (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+	#	Fix for arm/arm64 native builds on SBC boards with little RAM...
+	vcpkg_install_cmake(DISABLE_PARALLEL)
+else()
+	vcpkg_install_cmake()
+endif()
+
+if ("tool" IN_LIST FEATURES)
+	vcpkg_copy_tools(SEARCH_DIR ${CURRENT_PACKAGES_DIR}/bin/ TOOL_NAMES cct cs2cs geod gie proj projinfo projsync AUTO_CLEAN)
+	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+endif()
+
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/proj4)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+if ("tool" IN_LIST FEATURES)
+	file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
